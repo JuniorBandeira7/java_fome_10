62b384af6277cdb964839b5215c9ca3952792059
JuniorBandeira7 <juliobandeira77@gmail.com>
2025-05-03 10:00:58 -0300

    feat: defino email como unique

--- src/main/java/comida/loja/fome10/model/User.java
+++ src/main/java/comida/loja/fome10/model/User.java
@@ -1,5 +1,6 @@
 package comida.loja.fome10.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
     private String role;
